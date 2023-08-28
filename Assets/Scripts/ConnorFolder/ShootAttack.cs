fece04ec28c63f0b7eb7abb5776b98bbe8aa1c0c
ConCaerus <50085937+ConCaerus@users.noreply.github.com>
2023-08-28 08:55:37 -0500

    almost got shooting

--- Assets/Scripts/ConnorFolder/ShootAttack.cs
+++ Assets/Scripts/ConnorFolder/ShootAttack.cs
@@ -8,8 +8,9 @@ public class ShootAttack : Attack {
 
 
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space)) {
             attack();
+        }
     }
 
     public override attackType getAttackType() {
@@ -23,11 +24,22 @@ public class ShootAttack : Attack {
     public override void attack() {
         //  shoots a raycast FROM THE POSITION OF THE GUN (not from the player's position or the barrel of the gun) in the direction of the cursor
         var dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        RaycastHit hit;
+        var thing = Physics.Raycast(transform.position, dir, out hit, range == -1 ? Mathf.Infinity : range, LayerMask.GetMask("Enemy"));
+        // Does the ray intersect any objects excluding the player layer
+        if(thing) {
+            hit.collider.gameObject.GetComponent<Health>().takeDamage(getDamage());
+            Debug.Log("here");
+        }
+
+        Debug.DrawRay(transform.position, dir);
+
+        /*
         var hit = Physics2D.Raycast(transform.position, dir, range == -1 ? Mathf.Infinity : range, LayerMask.GetMask("Enemy"));
 
         if(hit.collider != null) {
             hit.collider.gameObject.GetComponent<Health>().takeDamage(getDamage());
-        }
+        }*/
     }
 
 }
