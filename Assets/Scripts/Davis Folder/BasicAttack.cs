0bc664cbc0213bd93d649677e5b6516ab0da45ef
Howi7 <71466823+Howi7@users.noreply.github.com>
2023-08-30 08:32:16 -0500

    Davis Folders plus frame start

---
+++ Assets/Scripts/Davis Folder/BasicAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasicAttack : Attack
+{
+    public override int getDamage()
+    {
+        return 12;
+    }
+    public override attackType getAttackType()
+    {
+        return attackType.Sword;
+    }
+    /*public override void attack(InputAction.CallbackContext c)
+    {
+        Debug.Log("Basic Attack Performed");
+    }*/
+
+    public override void attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
