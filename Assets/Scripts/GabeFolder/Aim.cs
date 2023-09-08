e2d0a84102818d629e6f875899894a37cd973663
Obliterine <73033572+obliterine@users.noreply.github.com>
2023-09-08 09:42:02 -0500

    Fixed Aim, made new Player Prefab
    I made a "Complete Player" Prefab that contains both of the cameras, as well as the base player prefab. I also fixed the disappearing issue.
    
    I also fixed the aim with the player in the middle of the screen.

--- Assets/Scripts/GabeFolder/Aim.cs
+++ Assets/Scripts/GabeFolder/Aim.cs
@@ -6,32 +6,25 @@ public class Aim : MonoBehaviour
 {
     [SerializeField]
     private float detectionDistance;
-    private Ray range;
-    [SerializeField]
-    private Transform originPosition;
     [SerializeField]
     private Camera cam;
 
     
     private Vector3 cursorPos;
-    private Vector3 cursorWorldPos;
+    private Vector3 cursorDir;
 
     private void Update()
     {
         //cursor into world space
         cursorPos = Input.mousePosition;
-        Ray ray = cam.ScreenPointToRay(cursorPos);
-        cursorWorldPos = ray.GetPoint(10);
 
         //raycast
-        range.direction = new Vector3(cursorWorldPos.x - originPosition.position.x, 
-                                      cursorWorldPos.y - 2*originPosition.position.y, 
-                                      cursorWorldPos.z - originPosition.position.z);
-        range.origin = transform.position;
-        Debug.DrawRay(range.origin, range.direction.normalized * detectionDistance, Color.red);
+        cursorDir = new Vector3(cursorPos.x - Screen.width / 2,
+                                     cursorPos.y - Screen.height / 2,
+                                     0);
 
         //rotation
-        float angle = Mathf.Atan2(range.direction.y, range.direction.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(cursorDir.y, cursorDir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
     }
 }
