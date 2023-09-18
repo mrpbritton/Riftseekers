bf6e7fd6844bfee53bec62c6ed1f0a2ccfbbfd1a
ConCaerus <connorkeeterheise@gmail.com>
2023-09-18 18:49:23 -0500

    fixed rocket aiming

--- Assets/Scripts/ConnorFolder/RocketLauncher.cs
+++ Assets/Scripts/ConnorFolder/RocketLauncher.cs
@@ -7,16 +7,18 @@ public class RocketLauncher : Attack {
 
     [SerializeField] GameObject rocketPreset;
     [SerializeField] float maxTravelDist, maxTravelTime;
+    [SerializeField] Transform rotTrans;
 
     public override void attack() {
         var curRocket = Instantiate(rocketPreset.gameObject);
         curRocket.GetComponent<RocketInstance>().setParentAttack(this, maxTravelTime);
         curRocket.transform.position = transform.position;
 
-        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var mPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0f, Input.mousePosition.y));
+        var dir = mPos - transform.position;
         dir.y = 0f;
-        dir.Normalize();
-        curRocket.transform.DOMove(dir * maxTravelDist, maxTravelTime);
+        Debug.Log(rotTrans.forward);
+        curRocket.transform.DOMove(transform.position + rotTrans.right * maxTravelDist, maxTravelTime);
         Destroy(curRocket.gameObject, maxTravelTime);
     }
     public override attackType getAttackType() {
