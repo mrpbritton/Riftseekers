b7a005f9eb49182cef72a40dee453bd9ea8a370a
ConCaerus <connorkeeterheise@gmail.com>
2023-09-19 14:08:17 -0500

    fixed shooting

--- Assets/Scripts/ConnorFolder/ShootAttack.cs
+++ Assets/Scripts/ConnorFolder/ShootAttack.cs
@@ -7,6 +7,8 @@ public class ShootAttack : Attack {
     [SerializeField] float range = -1f;
     [SerializeField] float damage = 12;
 
+    [SerializeField] Transform rotTrans;
+
 
     public override attackType getAttackType() {
         return attackType.Secondary;
@@ -17,21 +19,12 @@ public class ShootAttack : Attack {
     }
 
     public override void attack() {
-        Debug.Log("shot");
         //  shoots a raycast FROM THE POSITION OF THE GUN (not from the player's position or the barrel of the gun) in the direction of the cursor
-        var dir = (Vector3)(Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
-        if(Physics.Raycast(transform.position, dir, out hit, range == -1 ? Mathf.Infinity : range, LayerMask.GetMask("Enemy"))) {
+        if(Physics.Raycast(transform.position, rotTrans.right, out hit, range == -1 ? Mathf.Infinity : range, LayerMask.GetMask("Enemy"))) {
             hit.collider.gameObject.GetComponent<EnemyHealth>().damageTaken(getDamage());
         }
-
-        /* for 2D stuff if we have 2D stuff later
-        var hit = Physics2D.Raycast(transform.position, dir, range == -1 ? Mathf.Infinity : range, LayerMask.GetMask("Enemy"));
-
-        if(hit.collider != null) {
-            hit.collider.gameObject.GetComponent<Health>().takeDamage(getDamage());
-        }*/
     }
 
     protected override float getCooldownTime() {
