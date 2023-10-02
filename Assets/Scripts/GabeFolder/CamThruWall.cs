b5093548c6611402e98e27cecf4330dc25e7ff02
Obliterine <73033572+obliterine@users.noreply.github.com>
2023-10-02 09:01:40 -0500

    Edited CamThruWall
    It's working, but it needs to look good.

--- Assets/Scripts/GabeFolder/CamThruWall.cs
+++ Assets/Scripts/GabeFolder/CamThruWall.cs
@@ -6,17 +6,15 @@ public class CamThruWall : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
         if(other.CompareTag("wall"))
         {
             var mesh = other.gameObject.GetComponent<MeshRenderer>();
-            mesh.enabled = !mesh.enabled; //flip it
+            mesh.enabled = !mesh.enabled;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("BBBBBBBB");
         if (other.CompareTag("wall"))
         {
             var mesh = other.gameObject.GetComponent<MeshRenderer>();
