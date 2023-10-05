6d60a90e5ff74e24af91b33724755aeb1446804e
Howi72 <daviswhite30@gmail.com>
2023-10-04 19:52:37 -0500

    started on R ability
    started making the r ability make potentially will have to remake before build

--- Assets/Scripts/Davis Folder/rAbility.cs
+++ Assets/Scripts/Davis Folder/rAbility.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rAbility : Attack
+public class coneSlice : Attack
 {
+    public GameObject hurtbox;
+
+    public void Start()
+    {
+        //hurtbox.active = false;
+    }
     public override attackType getAttackType()
     {
         return attackType.FAbility;
@@ -11,7 +17,16 @@ public class rAbility : Attack
 
     public override void attack()
     {
-        throw new System.NotImplementedException();
+        hurtbox.SetActive(true);
+        Debug.LogError("Before");
+        inBetween();
+        Debug.LogError("After");
+        hurtbox.SetActive(false);
+    }
+
+    private IEnumerator inBetween()
+    {
+        yield return new WaitForSeconds(100000f);
     }
 
     protected override float getDamage()
