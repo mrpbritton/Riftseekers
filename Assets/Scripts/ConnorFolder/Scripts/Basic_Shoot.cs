5a1a519c53cafac7bccdeb2467cfd924574980ca
obliterine <73033572+obliterine@users.noreply.github.com>
2023-10-14 00:11:54 -0500

    Hitscan Shooting Implemented
    I made the hitscan shooting work, but honestly, I don't think it feels very nice.

--- Assets/Scripts/ConnorFolder/Scripts/Basic_Shoot.cs
+++ Assets/Scripts/ConnorFolder/Scripts/Basic_Shoot.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class Basic_Shoot : Attack {
     //  set to -1 for infinite
+
+    [Header("Gun Attributes")]
     [SerializeField, Tooltip("How far the bullet's raycast goes")] 
     float range = 10f;
     [SerializeField, Tooltip("Used in a calculation to see how much damage dealt")]
     float damage = 1f;
     [SerializeField, Tooltip("Used in a calculation to see how long the cooldown is in seconds")]
     float baseCooldown = 3f;
+    [Header("Trail Attributes")]
     [SerializeField, Tooltip("How fast the bullet trail disappears")]
-    float trailSpeed = 3f;
+    float trailSpeed = 0.05f;
     [SerializeField, Tooltip("Where the raycast comes from")]
     Transform origin;
     LineRenderer line;
@@ -22,64 +25,60 @@ public class Basic_Shoot : Attack {
     new private void Start()
     {
         //start and end point
+        base.Start();
+        line = GetComponent<LineRenderer>();
         line.positionCount = 2;
         line.enabled = false;
     }
 
-    public override attackType getAttackType() {
+    public override attackType getAttackType() 
+    {
         return attackType.Secondary;
     }
 
-    protected override float getDamage() {
+    protected override float getDamage() 
+    {
         return damage * frame.attackDamage;
     }
 
     public override void attack()
     {
-        Vector3 direction = Attack.GetPoint();
-        direction = new Vector3(direction.x, direction.y - origin.position.y + origin.localPosition.y, direction.z );
-        
+        Vector3 dir = Attack.GetPoint();
+        Vector3 direction = new Vector3(dir.x - origin.position.x + origin.localPosition.x, 
+                                        dir.y - origin.position.y + origin.localPosition.y, 
+                                        dir.z - origin.position.z + origin.localPosition.z);
+        /*Debug.Log($"dir: {dir} | direction: {direction}");*/
         Debug.DrawRay(origin.position, direction, Color.green, 5);
-        if(Physics.Raycast(origin.position, direction, out RaycastHit gotHit, range))
+        if(Physics.Raycast(origin.position, direction, out RaycastHit gotHit, range) && bCanShoot)
         {
-            if (gotHit.collider.gameObject.TryGetComponent(out EnemyHealth enemy) && bCanShoot)
+            if (gotHit.collider.gameObject.TryGetComponent(out EnemyHealth enemy))
             {
-                Debug.Log(true);
                 enemy.damageTaken(damage * frame.attackDamage);
-                StartCoroutine( DrawLine(origin.position, gotHit.point) );
-                StartCoroutine( AttackCooldown() );
             }
             else /*FindObjectOfType(explode)*/
             {
-                Debug.Log(false);
-
+                Debug.Log("AAA");
             }
+            StartCoroutine(DrawLine(origin.position, gotHit.point));
         }
-        //else: complete miss
+        else if (bCanShoot)
+        {
+            StartCoroutine(DrawLine(origin.position, dir));
+        }
     }
 
-    protected override float getCooldownTime() {
+    protected override float getCooldownTime() 
+    {
         return baseCooldown / frame.attackSpeed;
     }
 
-    /// <summary>
-    /// This uses attackSpeed for its cooldown
-    /// </summary>
-    /// <returns>Stops after a time in seconds</returns>
-    private IEnumerator AttackCooldown()
-    {
-        bCanShoot = false;
-        yield return new WaitForSeconds(baseCooldown / frame.attackSpeed);
-        line.enabled = false;
-        bCanShoot = true;
-    }
 
     private IEnumerator DrawLine(Vector3 origin, Vector3 point)
     {
+        line.enabled = true;
         line.SetPosition(0, origin);
         line.SetPosition(1, point);
-        line.enabled = true;
-        yield return new WaitForSeconds(0);
+        yield return new WaitForSeconds(trailSpeed);
         line.enabled = false;
     }
 }
