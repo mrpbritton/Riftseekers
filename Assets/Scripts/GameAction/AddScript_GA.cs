ae83cd3d35527294c85a73cec5751102e57aee4c
Obliterine <73033572+obliterine@users.noreply.github.com>
2023-10-18 19:33:15 -0500

    forgot to save a script

--- Assets/Scripts/GameAction/AddScript_GA.cs
+++ Assets/Scripts/GameAction/AddScript_GA.cs
@@ -2,28 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AddScript_GA : MonoBehaviour
+public class AddScript_GA : GameAction
 {
     public override void Action()
     {
-        Inventory.addItem(item);
-        Inventory.saveInventory();
-        //Debug.Log($"{Inventory.getItem(Inventory.getItemIndex(item), FindObjectOfType<ItemLibrary>()).title}");
-    }
-
-    /// <summary>
-    /// Adds an item i into the inventory.
-    /// </summary>
-    public void Action(ConItem i)
-    {
-        Inventory.addItem(i);
-        Inventory.saveInventory();
     }
 
     public override void DeAction()
     {
-        Inventory.removeItem(Inventory.getItemIndex(item));
-        Inventory.saveInventory();
+     
     }
 
     public override void ResetToDefault()
