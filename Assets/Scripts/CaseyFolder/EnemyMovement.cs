289043991061c99988caddd582b570b472b90e7b
EeriestPilot14 <casey.schriever@trojans.dsu.edu>
2023-10-20 08:33:31 -0500

    Fixed some enemy attacking interactions

--- Assets/Scripts/CaseyFolder/EnemyMovement.cs
+++ Assets/Scripts/CaseyFolder/EnemyMovement.cs
@@ -118,9 +118,9 @@ public class EnemyMovement : MonoBehaviour
     {
         if (bClose)
         {
-            meleeHit.SetActive(true);
+            meleeHit.GetComponent<Collider>().enabled = true;
             yield return new WaitForSeconds(0.5f);
-            meleeHit.SetActive(false);
+            meleeHit.GetComponent<Collider>().enabled = false;
         }
         yield return new WaitForSeconds(hitCooldown);
         meleeAttack();
