3ebcf614f9f1a24471121b8c1650a0a8af05a3ba
ConCaerus <50085937+ConCaerus@users.noreply.github.com>
2023-11-13 18:06:42 -0600

    player spawn hight

--- Assets/Scripts/ConnorFolder/Scripts/PlayerSpawnPos.cs
+++ Assets/Scripts/ConnorFolder/Scripts/PlayerSpawnPos.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerSpawnPos : MonoBehaviour {
     [SerializeField] GameObject defaultPlayer;
+    [SerializeField] float spawnHeight =1f;
 
     bool state = false;
 
     private void Awake() {
         //  checks if there's not a player in the scene
         if(FindObjectOfType<PlayerManager>() == null) {
-            var obj = Instantiate(defaultPlayer, transform.position, Quaternion.identity, null);
+            var obj = Instantiate(defaultPlayer, transform.position + Vector3.up * spawnHeight, Quaternion.identity, null);
             state = false;
         }
         else {
-            FindObjectOfType<PlayerManager>().getTallestParent().transform.position = transform.position;
+            FindObjectOfType<PlayerManager>().getTallestParent().transform.position = transform.position + Vector3.up * spawnHeight;
             var obj2 = FindObjectOfType<PlayerManager>();
             obj2.transform.localPosition = Vector3.zero;
             state = true;
