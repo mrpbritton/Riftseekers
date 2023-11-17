f80a9827cf9e7e1f3e5802cb2a572dfdde2e346a
ConCaerus <50085937+ConCaerus@users.noreply.github.com>
2023-11-17 08:14:36 -0600

    got inventory to work with controller

--- Assets/Scripts/ConnorFolder/Scripts/InventoryUI.cs
+++ Assets/Scripts/ConnorFolder/Scripts/InventoryUI.cs
@@ -46,7 +46,10 @@ public class InventoryUI : MonoBehaviour {
             }
         }
         else {
-
+            for(int i = 0; i < activeSlots.Count; i++)
+                activeSlots[i].GetComponent<Image>().color = i == actIndex ? Color.grey : Color.white;
+            for(int i = 0; i < inactiveSlots.Count; i++)
+                inactiveSlots[i].GetComponent<Image>().color = i == curIndex ? Color.grey : Color.white;
         }
     }
 
@@ -91,33 +94,46 @@ public class InventoryUI : MonoBehaviour {
     }
 
     void checkActiveItems(int masterInd, Attack.attackType overtype) {
+        if(overtype == Attack.attackType.None)
+            return;
         for(int i = 2; i >= 0; i--) {
-            if(i != masterInd && Inventory.getActiveItem(i, il).overrideAbil == overtype) { // move back to inv
-                var temp = Inventory.getActiveItem(i, il);
-                Inventory.removeActiveItem(i);
-                Inventory.addItem(temp);
+            if(i != masterInd) {
+                var it = Inventory.getActiveItem(i, il);
+                if(it != null && it.overrideAbil == overtype) { // move back to inv
+                    var temp = Inventory.getActiveItem(i, il);
+                    Inventory.removeActiveItem(i);
+                    Inventory.addItem(temp);
+                }
             }
         }
     }
 
 
     void swapState(bool usingKeyboard) {
+        curIndex = -1;
+        actIndex = -1;
+        dragged.gameObject.SetActive(false);
+        dragged.sprite = null;
         if(!usingKeyboard) {
             inactiveSlots[0].GetComponent<Button>().Select();
         }
     }
 
     public void setCurIndex(int ind) {
-        if(ind >= Inventory.getItems(il).Count) {
+        if(ind >= Inventory.getItems(il).Count && InputManager.isUsingKeyboard()) {
             return;
         }
 
         curIndex = ind;
-        draggedState = true;
-        dragged.gameObject.SetActive(true);
+        if(actIndex > -1)
+            swapItems(actIndex);
+        if(InputManager.isUsingKeyboard()) {
+            draggedState = true;
+            dragged.gameObject.SetActive(true);
 
-        dragged.gameObject.SetActive(true);
-        dragged.sprite = Inventory.getItems(il)[ind].image;
+            dragged.gameObject.SetActive(true);
+            dragged.sprite = Inventory.getItems(il)[ind].image;
+        }
     }
 
     public void setActIndex(int ind) {
@@ -142,8 +158,16 @@ public class InventoryUI : MonoBehaviour {
     }
 
     public void swapItems(int actInd) {
+        bool differentInds = actInd != actIndex;
         actIndex = actInd;
+        if(!InputManager.isUsingKeyboard() && (curIndex == -1 || actIndex == -1))
+            return;
         ConItem temp = Inventory.getActiveItem(actIndex, il);   //  saves the active item that's being replaced
+
+        //  checks if the items can be swapped (cannot have 2 or more active items with the same ability override type)
+        if(temp != null && differentInds && curIndex != -1)
+            checkActiveItems(actInd, temp.overrideAbil);
+
         //  overrides the active item if there is a valid overrider
         if(Inventory.getItems(il).Count > curIndex && curIndex != -1) {
             Inventory.overrideActiveItem(actIndex, Inventory.getItems(il)[curIndex]);
@@ -157,10 +181,10 @@ public class InventoryUI : MonoBehaviour {
         if(temp != null)
             Inventory.addItem(temp);
 
-        checkActiveItems(actInd, temp.overrideAbil);
         Inventory.saveInventory();
         applyActiveItemEffect(new List<ConItem>() { Inventory.getActiveItem(0, il), Inventory.getActiveItem(1, il), Inventory.getActiveItem(2, il) });
         show();
+        actIndex = -1;
         curIndex = -1;
     }
 }
