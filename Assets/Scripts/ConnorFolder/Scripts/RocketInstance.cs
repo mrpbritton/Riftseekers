2e43144d5348a4b8c9e7b42d73b819f7956fefe7
ConCaerus <50085937+ConCaerus@users.noreply.github.com>
2023-11-20 08:48:18 -0600

    made bullet isTrigger

--- Assets/Scripts/ConnorFolder/Scripts/RocketInstance.cs
+++ Assets/Scripts/ConnorFolder/Scripts/RocketInstance.cs
@@ -3,4 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RocketInstance : MonoBehaviour {
+    Coroutine expCo;
+    ExplosionManager em;
+    float expSize;
+
+
+    private void OnTriggerEnter(Collider col) {
+        if(!col.gameObject.CompareTag("Player") && !col.gameObject.CompareTag("Bullet")) {
+            if(!col.gameObject.CompareTag("Enemy")) {
+                AkSoundEngine.PostEvent("Object_Hit", gameObject);
+            }
+            StopCoroutine(expCo);
+            Debug.Log(col.gameObject.tag);
+            em.explode(transform.position, expSize, ExplosionManager.explosionState.HurtsEnemies);
+            Destroy(gameObject, 0.0001f);
+        }
+    }
+
+    public void setup(Coroutine ex, ExplosionManager e, float expS) {
+        expCo = ex;
+        em = e;
+        expSize = expS;
+    }
 }
