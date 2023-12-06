c90c57dce1c7bff7d2d8c0fa3c1cd384a98c302d
obliterine <73033572+obliterine@users.noreply.github.com>
2023-12-06 03:47:34 -0600

    Constrained sword
    sword is no longer free ranged

--- Assets/Scripts/GabeFolder/Attacks/GSword.cs
+++ Assets/Scripts/GabeFolder/Attacks/GSword.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,7 +18,7 @@ public class GSword : Attack
     private DoDamage damScript;
     private Vector3 direction; //what the direction currently is
     private Vector3 cachedDir; //filtered direction; cannot be zero
-
+    private CardinalDirection cardDir;
     protected override void Start()
     {
         base.Start();
@@ -37,7 +38,7 @@ public class GSword : Attack
 
     public override void anim()
     {
-
+        
     }
 
     public override void attack()
@@ -66,7 +67,10 @@ public class GSword : Attack
                                             dir.z - origin.position.z + origin.localPosition.z);
         }
         origin.forward = direction;
-        Debug.Log(Mathf.Atan2(origin.forward.z, origin.forward.x) * Mathf.Rad2Deg);
+        //Debug.Log(origin.forward);
+        float angle = Mathf.Atan2(origin.forward.z, origin.forward.x) * Mathf.Rad2Deg;
+        origin.forward = constrictDirection(angle);
+        //Debug.Log(Mathf.Atan2(origin.forward.z, origin.forward.x) * Mathf.Rad2Deg);
 
         if (damScript.damage != damage)
         {
@@ -75,6 +79,65 @@ public class GSword : Attack
 
         cooldownBar.updateSlider(getCooldownTime());
         StartCoroutine(Swing());
+    }
+
+    private Vector3 constrictDirection(float angle)
+    {
+        Vector3 returnVal = Vector3.zero;
+
+        if(angle < 22.5 && angle > -22.5) //East
+        {
+            returnVal = new Vector3(1, 0, 0);
+            cardDir = CardinalDirection.east;
+        }
+        else if(angle < 67.5 && angle > 22.5) //NE
+        {
+            //instead of doing exact math, im rounding sqrt(2)/2
+            returnVal = new Vector3(0.7071f, 0, 0.7071f);
+            cardDir = CardinalDirection.northEast;
+        }
+        else if (angle < 112.5 && angle > 67.5) //North
+        {
+            returnVal = new Vector3(0, 0, 1);
+            cardDir = CardinalDirection.north;
+        }
+        else if (angle < 157.5 && angle > 112.5) //NW
+        {
+            //instead of doing exact math, im rounding sqrt(2)/2
+            returnVal = new Vector3(-0.7071f, 0, 0.7071f);
+            cardDir = CardinalDirection.northWest;
+        }
+        else if (angle < -157.5 || angle > 157.5) //West
+        {
+            returnVal = new Vector3(-1, 0, 0);
+            cardDir = CardinalDirection.west;
+        }
+        else if (angle < -112.5 && angle > -157.5) //SW
+        {
+            //instead of doing exact math, im rounding sqrt(2)/2
+            returnVal = new Vector3(-0.7071f, 0, -0.7071f);
+            cardDir = CardinalDirection.southWest;
+        }
+        else if(angle < -67.5 && angle > -112.5) //South
+        {
+            returnVal = new Vector3(0, 0, -1);
+            cardDir = CardinalDirection.south;
+        }
+        else if(angle < -22.5 && angle > -67.5) //SE
+        {
+            //instead of doing exact math, im rounding sqrt(2)/2
+            returnVal = new Vector3(0.7071f, 0, -0.7071f);
+            cardDir = CardinalDirection.southEast;
+        }
+        else //default, East
+        {
+            returnVal = new Vector3(1, 0, 0);
+            cardDir = CardinalDirection.east;
+        }
+
+        //Debug.Log(cardDir.ToString());
+        
+        return returnVal;
     }
 
     IEnumerator Swing()
