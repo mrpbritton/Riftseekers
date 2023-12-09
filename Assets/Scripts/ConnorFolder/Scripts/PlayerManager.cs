b55aabb4b9ff662d34e072c370ef851cf803be9e
ConCaerus <50085937+ConCaerus@users.noreply.github.com>
2023-12-09 12:10:45 -0600

    removed old player spawning system

--- Assets/Scripts/ConnorFolder/Scripts/PlayerManager.cs
+++ Assets/Scripts/ConnorFolder/Scripts/PlayerManager.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
-    private void Awake() {
-        DontDestroyOnLoad(getTallestParent());
-    }
-
     public GameObject getTallestParent() {
         var tallestParent = gameObject;
         while(tallestParent.transform.parent != null)
