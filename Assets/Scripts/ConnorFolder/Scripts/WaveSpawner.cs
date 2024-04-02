8ea58d731c70962478cb0c5ba5dcd47f802c9fed
Obliterine <73033572+obliterine@users.noreply.github.com>
2024-04-02 18:59:04 -0500

    Added Delay, Changed Player hitbox, added initial wait

--- Assets/Scripts/ConnorFolder/Scripts/WaveSpawner.cs
+++ Assets/Scripts/ConnorFolder/Scripts/WaveSpawner.cs
@@ -50,6 +50,7 @@ public class WaveSpawner : Singleton<WaveSpawner> {
     }
 
     IEnumerator wave() {
+        yield return new WaitForSeconds(5f);
         while(true) {
             waveDone = false;
             for(int i = 0; i < monstersPerWave; i++) {
