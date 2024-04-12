f211eec5cbd29171e6cd3160c3f83b5432d1e63e
Obliterine <73033572+obliterine@users.noreply.github.com>
2024-04-12 14:23:01 -0500

    Update AddLore_GA.cs

--- Assets/Scripts/GameAction/AddLore_GA.cs
+++ Assets/Scripts/GameAction/AddLore_GA.cs
@@ -4,18 +4,10 @@ using UnityEngine;
 
 public class AddLore_GA : GameAction
 {
-    [Tooltip("LoreItem that provides information to be updated")]
-    public LorePiece_SO lore;
-
-    private void Start()
-    {
-    }
-
     public override void Action()
     {
-        //Add Lore to save stuff
-        AugmentLibrary.I.RemoveLore(lore);
-        //Save lore stuff
+        AugmentLibrary.I.getAndRemoveLore();
+        Inventory.saveInventory();
     }
 
     public override void DeAction()
