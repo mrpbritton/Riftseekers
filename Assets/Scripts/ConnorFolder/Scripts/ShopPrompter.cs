3ecabee3820fb8cae0b861f4602f0042e5e47736
ConCaerus <50085937+ConCaerus@users.noreply.github.com>
2024-04-26 08:43:04 -0500

    added controller support to shop?

--- Assets/Scripts/ConnorFolder/Scripts/ShopPrompter.cs
+++ Assets/Scripts/ConnorFolder/Scripts/ShopPrompter.cs
@@ -63,6 +63,7 @@ public class ShopPrompter : MonoBehaviour {
 
     void reshow() {
         moneyText.text = "Money: <color=yellow>" + Inventory.getMoney().ToString();
+        shopSlots[0].GetComponent<Button>().Select();
         //  shop
         for(int i = 0; i < shopSlots.Count; i++) {
             if(i < reference.items.Count) {
